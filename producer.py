2120b7496a4d36b29916f40a99cc90a6d31c1d49
Nick Nachefski <nnachefski@gmail.com>
2022-03-01 12:13:31 -0600

    updated

--- producer.py
+++ producer.py
@@ -9,7 +9,7 @@ parser.add_argument('kafka', action="store", default="kafka-hc-kafka-bootstrap.k
 parser.add_argument('-t', action="store", default=False, required=True, help='kafka topic')
 args = parser.parse_args()
 
-producer = KafkaProducer(bootstrap_servers=args.dest)
+producer = KafkaProducer(bootstrap_servers=args.kafka)
 letters = string.ascii_letters
 send_message = (''.join(random.choice(letters) for i in range(20)))
 print("sending '%s' to '%s'"%(send_message, args.topic))
