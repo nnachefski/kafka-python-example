61d68f424875496d0f28b384add78e94ed66c97e
Nick Nachefski <nnachefski@gmail.com>
2022-03-01 12:07:51 -0600

    updated

--- producer.py
+++ producer.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import string
-import random
+import sys, os, argparse, random, string
+from kafka import KafkaProducer
 
-# printing letters
+parser = argparse.ArgumentParser(description='Import images for disconnected OCP installs')
+parser.add_argument('kafka', action="store", default="kafka-hc-kafka-bootstrap.kafka.svc.cluster.local:9092", help='kafka bootstrap endpoint, Ex: kafka-hc-kafka-bootstrap.kafka.svc.cluster.local:9092')
+parser.add_argument('-t', action="topic", default=False, required=True, help='kafka topic')
+args = parser.parse_args()
+
+producer = KafkaProducer(bootstrap_servers=args.dest)
 letters = string.ascii_letters
-print(''.join(random.choice(letters) for i in range(20)))
+send_message = (''.join(random.choice(letters) for i in range(20)))
+print("sending '%s' to '%s'"%(send_message, args.dest))
+future = producer.send(args.topic, b"%s"%send_message)
+result = future.get(timeout=60)
