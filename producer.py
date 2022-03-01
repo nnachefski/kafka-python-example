1c4898f0b2c731f92fd7eb4ebc15d66208835e94
Nick Nachefski <nnachefski@gmail.com>
2022-03-01 11:39:40 -0600

    updated

---
+++ producer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import string
+import random
+
+# printing letters
+letters = string.ascii_letters
+print(''.join(random.choice(letters) for i in range(10)))
